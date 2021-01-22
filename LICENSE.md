5f2e3a0ca5b4dc7b7f0e647e3b67902d8a5371bb
milad noori <miladnoori@berkeley.edu>
2021-01-22 10:40:21 -0800

    adding license

--- LICENSE.md
+++ LICENSE.md
@@ -1,4 +1,3 @@
-'''
 Copyright (c) 2019 Boshen Zhang
 Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
 
@@ -7,45 +6,3 @@ Permission is hereby granted, free of charge, to any person obtaining a copy of 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-'''
-import os
-import sys
-import argparse
-from glob import glob
-
-# PROJ ROOT DIR
-DIR_PATH = os.path.dirname(os.path.abspath(__file__))
-ROOT_PATH = os.path.join(DIR_PATH, os.path.pardir)
-sys.path.append(ROOT_PATH)
-
-import pipeline.constants as const
-
-
-def get_latest_model(args):
-    model_path = const.SAVE_PATH    
-    models = glob(f"{model_path}/{const.DATASET}_{const.CAMERA_VIEW}_{const.BACKBONE_NAME}_{const.NUM_JOINTS}_a2j.pth")
-    return models[0]
-
-
-def get_arguments():
-    """Parse all the arguments provided from the CLI.
-    Returns:
-      A list of parsed arguments.
-    """
-    parser = argparse.ArgumentParser(description="Pipeline training")
-
-    parser.add_argument(
-        "--resume",
-        type=bool,
-        default=False,
-        help="set to True to resume training.",
-    )
-    
-    parser.add_argument(
-        "--resume_from_model",
-        type=str,
-        default=None,
-        help="Full path to a model checkpoint.",
-    )
-
-    return parser.parse_args()
--- pipeline/train.py
+++ pipeline/train.py
@@ -1,3 +1,13 @@
+'''
+Copyright (c) 2019 Boshen Zhang
+Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
 import os
 import sys
 import torch
@@ -29,22 +39,26 @@ def train(model_path):
         model_setup.epoch = epoch
         train_out = run_model(model_setup, train=True, test=False)
         model_setup.sched.step(epoch)
-        print(f"epoch: {epoch},\t"\
-                f"avg_loss: {train_out['avg_loss']:1.4f},\t"\
-                f"regression loss: {train_out['avg_reg_loss']:1.4f},\t"\
+        print(f"epoch: {epoch},	"\
+                f"avg_loss: {train_out['avg_loss']:1.4f},	"\
+                f"regression loss: {train_out['avg_reg_loss']:1.4f},	"\
                 f"classification loss: {train_out['avg_class_loss']:1.4f}"\
-                f"mean error: {train_out['mean_err'] : 1.4f},\t"\
-                f"max error: {train_out['max_err']:1.4f}\t"\
+                f"mean error: {train_out['mean_err'] : 1.4f},	"\
+                f"max error: {train_out['max_err']:1.4f}	"\
                 f"lr rate: {model_setup.sched.get_lr()[0]:1.9f}")
 
         if (epoch+1) % model_setup.save_freq == 0:
             train_val, mean_error_list, max_error_list = run_model(model_setup, train=False)
-            print(f"\n\nvalidation epoch: {epoch},\t"\
-                f"avg_loss validation: {train_val['avg_loss']:1.4f},\t"\
-                f"regression loss validation: {train_val['avg_reg_loss']:1.4f},\t"\
+            print(f"
+
+validation epoch: {epoch},	"\
+                f"avg_loss validation: {train_val['avg_loss']:1.4f},	"\
+                f"regression loss validation: {train_val['avg_reg_loss']:1.4f},	"\
                 f"classification loss validation: {train_val['avg_class_loss']:1.4f}"\
-                f"mean error: {train_val['mean_err'] : 1.4f},\t"\
-                f"max error: {train_val['max_err']:1.4f}\n\n")
+                f"mean error: {train_val['mean_err'] : 1.4f},	"\
+                f"max error: {train_val['max_err']:1.4f}
+
+")
             model_setup.save(epoch)
 
     
